8954d45735da27b8475e39f1a1f0a6e0235dcda5
faheemsaleem <fah03134508455@gmail.com>
2019-12-26 14:13:44 +0500

    some change

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import {youtubeLibraryLoaded} from './store/actions/api';
 import Trending from './containers/Trending/Trending';
 import Search from './containers/Search/Search';
 
-const API_KEY = '<enter your key here>';
+const API_KEY = 'AIzaSyAUH8MUT46r4uGCwR32D0jbfnl6EExE91s';
 
 class App extends Component {
   render() {
--- src/containers/SideBar/SideBarFooter/SideBarFooter.js
+++ src/containers/SideBar/SideBarFooter/SideBarFooter.js
@@ -7,19 +7,18 @@ export function SideBarFooter() {
       <div className='footer-block'>
         <div>About Press Copyright</div>
         <div>Creators Advertise</div>
-        <div>Developers +MyTube</div>
+        <div>Developers +Ewiglife</div>
         <div>Legal</div>
       </div>
       <div className='footer-block'>
         <div>Terms Privacy</div>
         <div>Policy & Safety</div>
-        <div>Test new features</div>
       </div>
       <div className='footer-block'>
-        <div>All prices include VAT</div>
+        <div>All prices include</div>
       </div>
       <div className='footer-block'>
-        <div>© Productioncoder.com - A Youtube clone for educational purposes under fair use.</div>
+        <div>©Ewiglife.com</div>
       </div>
     </React.Fragment>
   );
